57ac71a5f1b9276d2e64d53b147da56d61f52998
Zigao Wang <zigao.wang24@gmail.com>
2026-08-23 22:26:28 +0800

    fix: point canonical URLs at www.zigao.wang

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.zigao.wang',
+	site: 'https://www.zigao.wang',
 	integrations: [mdx(), sitemap()],
 });
